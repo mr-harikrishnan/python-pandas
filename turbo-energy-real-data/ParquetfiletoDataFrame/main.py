5038177ba6e19afb2b406ce32f3249297c74ab07
mr-harikrishnan <mr.harikrishnan85@gmail.com>
2026-09-10 12:51:59 +0530

    added real functions

--- turbo-energy-real-data/ParquetfiletoDataFrame/main.py
+++ turbo-energy-real-data/ParquetfiletoDataFrame/main.py
@@ -5,8 +5,15 @@ from scipy.stats import norm
 
 # pip install pandas fastparquet
 
-df = pd.read_parquet("./clickhouse_drift_calculation_computed.parquet")
+# df = pd.read_parquet("./final_torque_computed_drifting.parquet")
 
-df.to_csv("clickhouse_drift_calculation_computed.csv", index=False)
+# df.to_csv("converted_final_torque_computed_drifting.csv", index=False)
+
+
+df = pd.read_csv("./converted_final_torque_computed_drifting.csv")
+
+df = df.head(10)
+
+df.to_csv("10-drift-dats.csv",index=False)
 
 
